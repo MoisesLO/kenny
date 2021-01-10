31c8a9905b3037ce9e0f6a6f3039fc89a25358cb
moises <elnaufrago2009@gmail.com>
2021-01-09 22:03:46 -0500

    edit

---
+++ views/layouts/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Tailwind Css -->
+    <link href="/assets/css/tailwind.min.css" rel="stylesheet">
+
+    <!-- Material Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css" rel="stylesheet">
+
+    <!-- Alpine Js -->
+    <script src="/assets/js/alpine.min.js" defer></script>
+
+    <!-- Title -->
+    <title>Document</title>
+
+</head>
+<body>
+    
+
