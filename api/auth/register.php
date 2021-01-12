fa185c70259edfc3f105aacfaeb2b78ee8696ae5
Moises <Moises@DESKTOP-70VJT4J>
2021-01-11 22:06:45 -0500

    bd

--- api/auth/register.php
+++ api/auth/register.php
@@ -1,4 +1,15 @@
 <?php
-	$data = json_decode(file_get_contents("php://input"), TRUE);	
-	echo json_encode( $data );
+
+	$conn = new mysqli("localhost", "root", "", "kenny");
+	if ($conn->connect_error) {
+		die("ERROR: Unable to connect: " . $conn->connect_error);
+	} 
+
+	$insert	= "INSERT INTO users (nombres) VALUES ('moises)";
+	$conn->query($insert);
+	// $data = json_decode(file_get_contents("php://input"), TRUE);	
+	// echo json_encode( $data );
+
+	
+	
 ?>
