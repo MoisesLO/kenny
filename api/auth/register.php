c7ec6260640d4885e9900a1a344caf9ba470c7e6
Moises <Moises@DESKTOP-70VJT4J>
2021-01-13 01:39:29 -0500

    edit

--- api/auth/register.php
+++ api/auth/register.php
@@ -1,12 +1,14 @@
 <?php
 
-	$conn = new mysqli("localhost", "root", "", "kenny");
-	if ($conn->connect_error) {
-		die("ERROR: Unable to connect: " . $conn->connect_error);
-	} 
+	include '../../views/config/conn.php';
 
-	$insert	= "INSERT INTO users (nombres) VALUES ('moises)";
-	$conn->query($insert);
+	$registros = "INSERT INTO users (nombres) VALUES ('oscco')";
+    // $registros = $conn->query($registros)->fetch_all(MYSQLI_ASSOC);
+    $conn->query($registros);
+    // mysqli_query($conn, $registros)
+
+	// print_r($registros);
+	
 	// $data = json_decode(file_get_contents("php://input"), TRUE);	
 	// echo json_encode( $data );
 
--- views/config/conn.php
+++ views/config/conn.php
@@ -1,17 +1,9 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
 
     $conn = new mysqli("localhost", "root", "", "kenny");
     if ($conn->connect_error) {
         die("ERROR: Unable to connect: " . $conn->connect_error);
     } 
 
-    $registros = "INSERT INTO users (nombres) VALUES ('linares')";
-    // $registros = $conn->query($registros)->fetch_all(MYSQLI_ASSOC);
-    $conn->query($registros);
-    // mysqli_query($conn, $registros)
-
-    // print_r($registros);
+    
 ?>
