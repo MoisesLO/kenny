08d135e583df7484009182dfe5a20237a21cbaf0
moises <elnaufrago2009@gmail.com>
2021-01-15 20:50:07 -0500

    edit

--- views/config/conn.php
+++ views/config/conn.php
@@ -2,7 +2,5 @@
     $conn = new mysqli("localhost", "root", "moiseslinar3s", "kenny");
     if ($conn->connect_error) {
         die("ERROR: Unable to connect: " . $conn->connect_error);
-    } 
-
-    
+    }     
 ?>
