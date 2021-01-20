5630816dd80429f96dda39e11d14deeb6136e3b3
moises <elnaufrago2009@gmail.com>
2021-01-19 19:23:23 -0500

    edit

--- api/docs/list.php
+++ api/docs/list.php
@@ -1,3 +1,8 @@
 <?php 
-    
+    $documents = [
+        [ "serie" => "F001", "numero" => "54768"],
+        [ "serie" => "F001", "numero" => "54768"],
+    ];
+
+    echo json_encode($documents);
 ?>
