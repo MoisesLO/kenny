382020b7804e7429615b298123b2e73314f04309
Moises <Moises@DESKTOP-70VJT4J>
2021-01-20 01:08:28 -0500

    edit

--- api/docs/list.php
+++ api/docs/list.php
@@ -1,7 +1,11 @@
 <?php 
     $documents = [
-        [ "serie" => "F001", "numero" => "54768"],
-        [ "serie" => "F001", "numero" => "54768"],
+        [ "serie" => "F001", "numero" => "54768", "razon" => "SURMOTRIZ SOCIEDAD ANONIMA", "documento" => "1042516253", "fecha" => "19-01-2021", "estado" => "enviado", "monto" => "863.45"],
+        [ "serie" => "F002", "numero" => "54768", "razon" => "SURMOTRIZ SOCIEDAD ANONIMA", "documento" => "1042516253", "fecha" => "19-01-2021", "estado" => "enviado", "monto" => "863.45"],
+        [ "serie" => "F003", "numero" => "54768", "razon" => "SURMOTRIZ SOCIEDAD ANONIMA", "documento" => "1042516253", "fecha" => "19-01-2021", "estado" => "enviado", "monto" => "863.45"],
+        [ "serie" => "F004", "numero" => "54768", "razon" => "SURMOTRIZ SOCIEDAD ANONIMA", "documento" => "1042516253", "fecha" => "19-01-2021", "estado" => "enviado", "monto" => "863.45"],
+        [ "serie" => "F004", "numero" => "54768", "razon" => "SURMOTRIZ SOCIEDAD ANONIMA", "documento" => "1042516253", "fecha" => "19-01-2021", "estado" => "enviado", "monto" => "863.45"],
+        [ "serie" => "F005", "numero" => "54768", "razon" => "SURMOTRIZ SOCIEDAD ANONIMA", "documento" => "1042516253", "fecha" => "19-01-2021", "estado" => "enviado", "monto" => "863.45"]
     ];
 
     echo json_encode($documents);
