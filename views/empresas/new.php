f297418f0cb2b611322cafd49f53bb05e5310ec2
moises <elnaufrago2009@gmail.com>
2021-02-05 11:29:08 -0500

    edit

--- views/empresas/new.php
+++ views/empresas/new.php
@@ -10,7 +10,7 @@
   <main class="flex-1 flex flex-col bg-gray-50 dark:bg-gray-700 transition duration-500 ease-in-out overflow-y-auto">
     <div class="mx-10 my-2">
       <nav
-        class="flex flex-row justify-between border-b dark:border-gray-600 dark:text-gray-400 transition duration-500 ease-in-out">
+          class="flex flex-row justify-between border-b dark:border-gray-600 dark:text-gray-400 transition duration-500 ease-in-out">
         <div class="flex">
           <!-- Top NavBar -->
 
@@ -19,11 +19,11 @@
             Lista
           </a>
           <button
-            class="ml-6 py-2 block border-b-2 border-transparent focus:outline-none font-medium capitalize text-center focus:text-green-500 focus:border-green-500 dark-focus:text-green-200 dark-focus:border-green-200 transition duration-500 ease-in-out">
+              class="ml-6 py-2 block border-b-2 border-transparent focus:outline-none font-medium capitalize text-center focus:text-green-500 focus:border-green-500 dark-focus:text-green-200 dark-focus:border-green-200 transition duration-500 ease-in-out">
             Nuevo
           </button>
           <button
-            class="ml-6 py-2 block border-b-2 border-transparent focus:outline-none font-medium capitalize text-center focus:text-green-500 focus:border-green-500 dark-focus:text-green-200 dark-focus:border-green-200 transition duration-500 ease-in-out">
+              class="ml-6 py-2 block border-b-2 border-transparent focus:outline-none font-medium capitalize text-center focus:text-green-500 focus:border-green-500 dark-focus:text-green-200 dark-focus:border-green-200 transition duration-500 ease-in-out">
             Inicio
           </button>
         </div>
@@ -52,108 +52,147 @@
 
       <!-- Titulo -->
       <div class="flex justify-between items-center pt-9">
-        <h2 class="my-4 pb-4 text-3xl font-semibold text-gray-600">
+        <h2 class="my-4 pb-4 text-3rdmxl font-semibold text-gray-600">
           <i class="fas fa-plus"></i> Nueva Empresa
         </h2>
       </div>
 
       <!-- Contenido -->
-      <div class="md:w-1/2 px-16 py-10 rounded-lg shadow mb-16">
+      <div class="md:w-1/2 px-16 py-10 rounded-lg shadow mb-16" x-data="data()">
 
-        <!-- Razon Social -->
-        <div class="flex">
-          <div class="w-full mb-5">
-            <label for="" class="text-sm font-semibold text-gray-500 px-1">Razon Social</label>
-            <div class="flex mt-1">
-              <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                <i class="fas fa-file-alt text-gray-400 text-lg"></i>
+        <form @prevent.>
+          <!-- Razon Social -->
+          <div class="flex">
+            <div class="w-full mb-5">
+              <label for="" class="text-sm font-semibold text-gray-500 px-1">Razon Social</label>
+              <div class="flex mt-1">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                  <i class="fas fa-file-alt text-gray-400 text-lg"></i>
+                </div>
+                <input type="text"
+                       x-model="empresa.razon"
+                       class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-400"
+                       placeholder="">
               </div>
-              <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-400" placeholder="">
             </div>
           </div>
-        </div>
 
-        <!-- RUC -->
-        <div class="flex">
-          <div class="w-full mb-5">
-            <label for="" class="text-sm font-semibold text-gray-500 px-1">RUC</label>
-            <div class="flex mt-1">
-              <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                <i class="fas fa-sort-numeric-up-alt text-gray-400 text-lg"></i>
+          <!-- RUC -->
+          <div class="flex">
+            <div class="w-full mb-5">
+              <label for="" class="text-sm font-semibold text-gray-500 px-1">RUC</label>
+              <div class="flex mt-1">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                  <i class="fas fa-sort-numeric-up-alt text-gray-400 text-lg"></i>
+                </div>
+                <input type="text"
+                       x-model="empresa.ruc"
+                       class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500"
+                       placeholder="">
               </div>
-              <input type="email" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500" placeholder="">
             </div>
           </div>
-        </div>
 
-        <!-- Direccion -->
-        <div class="flex">
-          <div class="w-full mb-5">
-            <label for="" class="text-sm font-semibold text-gray-500 px-1">Direccion</label>
-            <div class="flex mt-1">
-              <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                <i class="fas fa-address-card text-gray-400 text-lg"></i>
+          <!-- Direccion -->
+          <div class="flex">
+            <div class="w-full mb-5">
+              <label for="" class="text-sm font-semibold text-gray-500 px-1">Direccion</label>
+              <div class="flex mt-1">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                  <i class="fas fa-address-card text-gray-400 text-lg"></i>
+                </div>
+                <input type="text"
+                       x-model="empresa.direccion"
+                       class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500"
+                       placeholder="">
               </div>
-              <input type="email" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500" placeholder="">
             </div>
           </div>
-        </div>
 
-        <!-- Correo -->
-        <div class="flex">
-          <div class="w-full mb-5">
-            <label for="" class="text-sm font-semibold text-gray-500 px-1">Correo</label>
-            <div class="flex mt-1">
-              <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                <i class="fas fa-at text-gray-400 text-lg"></i>
+          <!-- Correo -->
+          <div class="flex">
+            <div class="w-full mb-5">
+              <label for="" class="text-sm font-semibold text-gray-500 px-1">Correo</label>
+              <div class="flex mt-1">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                  <i class="fas fa-at text-gray-400 text-lg"></i>
+                </div>
+                <input type="email"
+                       x-model="empresa.correo"
+                       class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500"
+                       placeholder="">
               </div>
-              <input type="email" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500" placeholder="">
             </div>
           </div>
-        </div>
 
-        <!-- Telefono -->
-        <div class="flex">
-          <div class="w-full mb-5">
-            <label for="" class="text-sm font-semibold text-gray-500 px-1">Telefono</label>
-            <div class="flex mt-1">
-              <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                <i class="fas fa-phone text-gray-400 text-lg"></i>
+          <!-- Telefono -->
+          <div class="flex">
+            <div class="w-full mb-5">
+              <label for="" class="text-sm font-semibold text-gray-500 px-1">Telefono</label>
+              <div class="flex mt-1">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                  <i class="fas fa-phone text-gray-400 text-lg"></i>
+                </div>
+                <input type="email"
+                       x-model="empresa.telefono"
+                       class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500"
+                       placeholder="">
               </div>
-              <input type="email" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500" placeholder="">
             </div>
           </div>
-        </div>
 
-        <!-- Estado -->
-        <div class="flex">
-          <div class="w-full mb-10">
-            <label for="" class="text-sm font-semibold text-gray-500 px-1">Estado</label>
-            <div class="flex mt-1">
-              <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                <i class="fas fa-check-circle text-gray-400 text-lg"></i>
+          <!-- Estado -->
+          <div class="flex">
+            <div class="w-full mb-10">
+              <label for="" class="text-sm font-semibold text-gray-500 px-1">Estado</label>
+              <div class="flex mt-1">
+                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                  <i class="fas fa-check-circle text-gray-400 text-lg"></i>
+                </div>
+                <select
+                    x-model="empresa.estado"
+                    class="w-full py-2 -ml-10 pl-10 rounded-lg pl-2 text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500">
+                    <option value="1">Activo</option>
+                    <option value="2">Desactivado</option>
+                </select>
               </div>
-              <select class="w-full py-2 -ml-10 pl-10 rounded-lg pl-2 text-gray-600 border-2 border-gray-200 outline-none focus:border-gray-500">
-                <option value="1">Activo</option>
-                <option value="1">Desactivado</option>
-              </select>
             </div>
           </div>
-        </div>
 
-        <div class="flex">
-          <div class="w-full">
-            <button class="w-full rounded-lg py-2 border-2 border-gray-400 text-white bg-gray-500 hover:bg-gray-700">
-              <i class="fas fa-save text-lg"></i> Guardar
-            </button>
+          <div class="flex">
+            <div class="w-full">
+              <button type="submit" class="w-full rounded-lg py-2 border-2 border-gray-400 text-white bg-gray-500 hover:bg-gray-700">
+                <i class="fas fa-save text-lg"></i> Guardar
+              </button>
+            </div>
           </div>
-        </div>
+
+        </form>
 
       </div>
 
     </div>
   </main>
 </div>
+
+<script type="text/javascript">
+  function data() {
+    return {
+      empresa: {
+        razon: 'Abraham Moises Linares Oscco',
+        ruc: '237675239873',
+        direccion: 'Ct 40 Lt 15 Mz 213 Ciudad Nueva',
+        correo: 'elnaufrago2009@gmail.com',
+        telefono: '95264876',
+        estado: '2'
+      }
+    }
+  }
+</script>
+
 </body>
+
+<!-- Footer -->
+<?php include "../layouts/footer_.php" ?>
 
 
